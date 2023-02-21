66624311194350d8580464d33a0f4b0ba6c8bc46
Cerua11 <acecko11@wp.pl>
2023-02-21 21:49:33 +0100

    big upgrade, songs cards, general design, class active and start coding in JS

--- src/global-scripts/main.js
+++ src/global-scripts/main.js
@@ -1,5 +1,16 @@
 /// Main JavaScript File
 /// Here we import all the global JavaScript files we need for our project.
 
-import '../global-styles/style.scss'
+class SpotifyFunctions{
+    constructor(){
+        const heart = document.getElementsByTagName(".spotify-container__content-options__heart");
+        this.heart.addEventListener("click", () =>{
+            console.log("elo");
+            this.heart.style.backgroundColor = "red";
+        });
+    }
+   
+    };
 
+
+const Spotify = new SpotifyFunctions();
